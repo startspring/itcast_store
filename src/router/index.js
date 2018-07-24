05094088d9741efff596e278d0cde8c4d26ff301
[zcx] <[1787697839@qq.com]>
2018-07-24 17:01:03 +0800

    home组件和路由

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import Router from 'vue-router';
 
 // 导入登录组件
 import Login from '@/views/login';
+// 导入home组件
+import Home from '@/views/home';
 
 Vue.use(Router);
 
@@ -10,6 +12,7 @@ export default new Router({
   routes: [
     // 添加默认路由
     { name: 'l', path: '/', redirect: { name: 'login' } },
-    { name: 'login', path: '/login', component: Login }
+    { name: 'login', path: '/login', component: Login },
+    { name: 'home', path: '/home', component: Home } 
   ]
 });
